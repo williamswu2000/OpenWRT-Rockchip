97247126b064b9f931997aecdd1b85a8f1179a63
quintus-lab <31897806+quintus-lab@users.noreply.github.com>
2021-03-08 00:14:44 +0800

    refresh 21.02

---
+++ step/00-prepare_openwrt.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+clear
+#Update feed
+sed -i '4s/src-git/#src-git/g' ./feeds.conf.default
+sed -i '5s/src-git/#src-git/g' ./feeds.conf.default
+echo 'src-git addon https://github.com/quintus-lab/openwrt-package' >> ./feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
+
+#patch jsonc
+patch -p1 < ../patches/0000-use_json_object_new_int64.patch
+#Add upx-ucl support
+patch -p1 < ../patches/0001-tools-add-upx-ucl-support.patch
+#add hwrng patch
+patch -p1 < ../patches/0002-rockchip-rngd.patch
+
+# add R4S support
+patch -p1 < ../patches/0003-new_rockchip_support_k54.patch
+patch -p1 < ../patches/0007-optimize_for_rk3399.patch
+
+# add AES and GCM with ARMv8 Crypto support
+patch -p1 < ../patches/0008-mbedtls-Implements-AES-and-GCM-with-ARMv8-Crypto-Ext.patch
+
+#dnsmasq aaaa filter
+patch -p1 < ../patches/1001-dnsmasq_add_filter_aaaa_option.patch
+cp -f ../patches/910-mini-ttl.patch package/network/services/dnsmasq/patches/
+cp -f ../patches/911-dnsmasq-filter-aaaa.patch package/network/services/dnsmasq/patches/
+
+#Fullcone patch
+patch -p1 < ../patches/1002-add-fullconenat-support.patch
+patch -p1 < ../patches/1003-luci-app-firewall_add_fullcone.patch
+
+#update curl
+rm -rf ./package/network/utils/curl
+svn co https://github.com/openwrt/openwrt/branches/openwrt-19.07/package/network/utils/curl package/network/utils/curl
+
+#Max connection limite
+sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+
+exit 0
